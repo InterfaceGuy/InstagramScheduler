d163eaa8ddb4ecab02b260eb24f6f75cf7a4841b
InterfaceGuy <65857060+InterfaceGuy@users.noreply.github.com>
2025-05-15 12:27:33 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://yourusername.github.io',
+  base: '/dreamnode-instagram-scheduler',
 });
